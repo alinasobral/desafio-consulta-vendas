d56df4cfc6c6e21ccb60cc827b1813dac68c3509
alinasobral <alinasobral@hotmail.com>
2026-04-27 20:03:20 -0300

    feat: implementa relatório e sumário de vendas com JPQL

--- src/main/java/com/devsuperior/dsmeta/dto/SaleMinDTO.java
+++ src/main/java/com/devsuperior/dsmeta/dto/SaleMinDTO.java
@@ -7,19 +7,23 @@ import com.devsuperior.dsmeta.entities.Sale;
 public class SaleMinDTO {
 
 	private Long id;
-	private Double amount;
 	private LocalDate date;
+	private Double amount;
+	private String sellerName;
+
 	
-	public SaleMinDTO(Long id, Double amount, LocalDate date) {
+	public SaleMinDTO(Long id, LocalDate date, Double amount, String sellerName) {
 		this.id = id;
 		this.amount = amount;
 		this.date = date;
+		this.sellerName = sellerName;
 	}
 	
 	public SaleMinDTO(Sale entity) {
 		id = entity.getId();
 		amount = entity.getAmount();
 		date = entity.getDate();
+		sellerName = entity.getSeller().getName();
 	}
 
 	public Long getId() {
@@ -32,5 +36,9 @@ public class SaleMinDTO {
 
 	public LocalDate getDate() {
 		return date;
+	}
+
+	public String getSellerName() {
+		return sellerName;
 	}
 }
